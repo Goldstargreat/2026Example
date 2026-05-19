b5c076c2b6e117643fcaecc4280c42585e9cf701
goldstargreat <goldstargreat@gmail.com>
2026-05-19 11:00:59 +0900

    오류 수정

--- src/main/java/kr/ac/kopo/gnuyog/_026example/controller/Ex08Controller.java
+++ src/main/java/kr/ac/kopo/gnuyog/_026example/controller/Ex08Controller.java
@@ -6,12 +6,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Ex08Controller {
+
     @GetMapping("/exam08")
-    public ModelAndView requestMethod(){
-        ModelAndView modeView = new ModelAndView();
-        modeView.addObject("title1", "ModelAndView 연습");
-        modeView.addObject("title2", "ModelAndView는 addObject()와 setViewName()를 사용");
-        modeView.setViewName("view06_1");
-        return modeView;
+    public ModelAndView requestMethod() {
+
+        // ModelAndView 객체 생성
+        ModelAndView modelView = new ModelAndView();
+
+        // 데이터 저장
+        modelView.addObject("title1", "ModelAndView 연습");
+        modelView.addObject("title2",
+                "ModelAndView는 addObject()와 setViewName()를 사용");
+
+        // 이동할 뷰 페이지 지정
+        modelView.setViewName("viewPage");
+
+        // ModelAndView 객체 반환
+        return modelView;
     }
 }
