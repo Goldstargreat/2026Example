2b4ab5dbdabeda19d3946f2ddf96b1370aa2b42c
goldstargreat <goldstargreat@gmail.com>
2026-06-09 09:56:11 +0900

    Security와 관련된 의존성 추가, 접근 권한 설정, 암호화 설정, 사용자 정보 등록을 위한 환경 설정 클라스 작성

--- src/main/java/kr/ac/kopo/gnuyog/_026example/domain/Member2.java
+++ src/main/java/kr/ac/kopo/gnuyog/_026example/domain/Member2.java
@@ -9,6 +9,6 @@ public class Member2
     @MemberId
     private String memberId;
 
-    @Size(min = 4, message = "비밀번호는 4 ~ 10자 이내의 문자열로 입력해주세요.")
+    @Size(min = 4, max =10, message = "비밀번호는 4 ~ 10자 이내의 문자열로 입력해주세요.")
     private String passwd;
 }
