600ddac09bfe616d4a61521153f61b31afa6ea63
goldstargreat <goldstargreat@gmail.com>
2026-06-09 11:57:34 +0900

    사용자 정보 검색 유형 중에 authentication 유형을 사용함

--- src/main/java/kr/ac/kopo/gnuyog/_026example/controller/Chap10_01Controller.java
+++ src/main/java/kr/ac/kopo/gnuyog/_026example/controller/Chap10_01Controller.java
@@ -6,33 +6,36 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
 
 @Controller
+@RequestMapping("/exam10_01")
 public class Chap10_01Controller
 {
-    @GetMapping("/exam10_01/exam01")
+
+    @GetMapping("/exam01")
     public String requestMethod(Model model)
     {
         return "viewPage10_01";
     }
 
-    @GetMapping("/exam10_01/admin/main")
+    @GetMapping("/admin/main")
     public String requestMethod2(Model model)
     {
         model.addAttribute("viewName", "adminPage.html");
         return "adminPage";
     }
 
-    @GetMapping("/exam10_01/manager/main")
+    @GetMapping("/manager/main")
     public String requestMethod3(Model model)
     {
         model.addAttribute("viewName", "managerPage.html");
         return "managerPage";
     }
 
-    @GetMapping("/exam10_01/member/main")
+    @GetMapping("/member/main")
     public String requestMethod4(Model model)
     {
         model.addAttribute("viewName", "memberPage.html");
@@ -42,32 +45,29 @@ public class Chap10_01Controller
     @GetMapping("/home/main")
     public String requestMethod5(Model model)
     {
-        model.addAttribute("viewName", "homePage.html");
         return "homePage";
     }
 
     @GetMapping("/exam02")
     public String requestMethod6(Model model)
     {
-        return "redirect:/member/user";
+        return "redirect:/exam10_01/member/user";
     }
 
     @GetMapping("/member/user")
-    public String requestMethod7(Authentication authentication, Model model)
-    {
+    public String requestMethod7(Authentication authentication, Model model){
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
         String userName = userDetails.getUsername();
         String password = userDetails.getPassword();
-
-        model.addAttribute("username", userName);
+        model.addAttribute("userName", userName);
         model.addAttribute("password", password);
         model.addAttribute("uri", "/member/user");
         Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-        for(GrantedAuthority item : authorities)
+        for (GrantedAuthority item : authorities)
         {
             model.addAttribute("role", item + " ");
         }
-        return "memberPage";
+
+        return "viewPage10_02";
     }
 }
