6d700f351271a9b914f971ada1887d7753804080
goldstargreat <goldstargreat@gmail.com>
2026-08-18 10:46:19 +0900

    @ExceptionHandler를 사용한 예외처리 방법

--- src/main/java/kr/ac/kopo/gnuyog/_026example/controller/Chap11_01Controller.java
+++ src/main/java/kr/ac/kopo/gnuyog/_026example/controller/Chap11_01Controller.java
@@ -3,6 +3,8 @@ package kr.ac.kopo.gnuyog._026example.controller;
 import kr.ac.kopo.gnuyog._026example.exception.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,9 +19,30 @@ public class Chap11_01Controller
     {
         System.out.println(new IllegalArgumentException("요청에 실패했습니다.").getMessage());
     }
+
     @GetMapping("/exam02")
     public void requestMethod02() throws Exception
     {
         throw new Exception(new UserException("UserException 메시지입니다."));
+    }
+
+    @GetMapping("/exam03")
+    public void requestMethod03()
+    {
+        throw new UserException("UserException @ExceptionHandler 예제 메시지입니다.");
+    }
+
+    @ExceptionHandler(UserException.class)
+    public String handlerException(UserException ex, Model model)
+    {
+        model.addAttribute("data1", ex.getMessage());
+        model.addAttribute("data2", ex);
+        return "viewPage11_01";
+    }
+
+    @GetMapping("/exam04")
+    public void requestMethod04()
+    {
+        throw new UserException("UserException @ControllerAdvice 예제 메시지입니다. ");
     }
 }
